b69d8cfb2e73f2bc84fe78015c8aa19b37e4eb60
soural1417 <soural1417@gmail.com>
2020-08-31 17:45:39 +0530

    zh

--- src/App.js
+++ src/App.js
@@ -61,7 +61,11 @@ function Home() {
     fontFamily: "Playfair Display"}}>Creating opportunities for better health decisions</h4><br></br>
     <Link className="search" to="/about">Start Here</Link>
     <h6 style={{paddingTop:"0px",color:"#fff", width:"300px",textAlign:"center",letterSpacing: "0.05em",
-    fontFamily: "Playfair Display"}}>Highly recommended to use downloaded version. In case the download button could not be found, select the Add to the home screen button from the three dots from the browser</h6><br></br>
+    fontFamily: "Playfair Display"}}>We highly recommend that you use the downloaded version. To download the app, Click the Download now (at the bottom screen) → Add to home screen. In case the Download button does not appear on the screen:
+    For android: 
+    Click ( ) on the top right corner of the screen → Add to home screen
+    For iOS:
+    Click ( ) on the bottom of the screen → Add to home screen</h6><br></br>
 
 </div>
   );
