62f2e0a9a136ad608a0fbd77293c5b339a470c7b
soural1417 <soural1417@gmail.com>
2020-08-31 18:23:31 +0530

    jhv

--- src/App.js
+++ src/App.js
@@ -61,9 +61,7 @@ function Home() {
     fontFamily: "Playfair Display"}}>Creating opportunities for better health decisions</h4><br></br>
     <Link className="search" to="/about">Start Here</Link>
     <h6 style={{paddingTop:"0px",color:"#fff", width:"300px",textAlign:"center",letterSpacing: "0.05em",
-    fontFamily: "Playfair Display"}}>We highly recommend that you use the downloaded version.</h6><h6 style={{paddingTop:"0px",color:"#fff", width:"300px",textAlign:"center",letterSpacing: "0.05em",
-    fontFamily: "Playfair Display"}}>To download the app, Click the Download now (at the bottom screen) → Add to home screen.</h6><h6 style={{paddingTop:"0px",color:"#fff", width:"300px",textAlign:"center",letterSpacing: "0.05em",
-    fontFamily: "Playfair Display"}}> In case the Download button does not appear on the screen:
+    fontFamily: "Playfair Display"}}>We highly recommend that you use the downloaded version. To download the app, Click the Download now (at the bottom screen) → Add to home screen. In case the Download button does not appear on the screen:
    </h6><h6 style={{paddingTop:"0px",color:"#fff", width:"300px",textAlign:"center",letterSpacing: "0.05em",
     fontFamily: "Playfair Display"}}> For android: 
    </h6><h6 style={{paddingTop:"0px",color:"#fff", width:"300px",textAlign:"center",letterSpacing: "0.05em",
